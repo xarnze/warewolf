bd9799079c3d20c5ce95c6bff65bdead18b1b7ff
Aaron Polley <xarnze@gmail.com>
2017-12-25 18:49:53 +0000

    Placeholders for instant kill actions and vote to kill actions

--- GameScreens/Action.js
+++ GameScreens/Action.js
@@ -12,7 +12,7 @@ export class Action extends React.Component {
     this.state = {
       // toggle box is closed initially
       show: false,
-      disableKilling: false,
+      nextEnabled: AppData.Characters[AppData.Game.LivePlayers[AppData.Game.ActivePlayer].characterId].kills,
       item: "Select Item",
       isVisible: true,
     };
@@ -33,7 +33,7 @@ export class Action extends React.Component {
           <View style={AppData.Characters[AppData.Game.LivePlayers[AppData.Game.ActivePlayer].characterId].kills ? styles.listContainer : { display: 'none' }} >
             <Picker
               selectedValue={this.state.item}
-              onValueChange={(itemValue, itemIndex) => this.setState({item: itemValue})}>
+              onValueChange={(itemValue, itemIndex) => this.setState({item: itemValue, nextEnabled: true})}>
               {AppData.Game.LivePlayers.map(function(a) {
                    return (
                        <Picker.Item label={a.name} value={a.name}/>
@@ -42,7 +42,7 @@ export class Action extends React.Component {
             </Picker>
           </View>
           <Button
-            title='Next' onPress={ () => { this.next(dataFunc) } } />
+            title='Next' disabled={this.state.nextEnabled} onPress={ () => { this.next(dataFunc) } } />
         </View>
       </View>
     );
@@ -54,20 +54,40 @@ export class Action extends React.Component {
     }
     return players;
   }
+  getPlayerIdxByName(playerName, playerList){
+    for (var i = 0; i < playerList.length; i++) {
+      const player = playerList[i];
+      if(player.name === playerName){
+        return i;
+      }
+    }
+    return -1;
+  }
   killPlayer(playerIdx, dataFunc) {
     /*let appData = dataFunc();
     appData.Game.LivePlayers.splice(playerIdx, 1)
     dataFunc(appData)*/
   }
+  markPlayerForWolfAttack(playerIdx, dataFunc) {
+
+  }
+  getKilledByWolf(dataFunc) {
+
+  }
   next(dataFunc) {
     let appData = dataFunc();
-    console.log(appData.Game.LivePlayers.length);
+    if(AppData.Characters[AppData.Game.LivePlayers[AppData.Game.ActivePlayer].characterId].kills && !AppData.Characters[AppData.Game.LivePlayers[AppData.Game.ActivePlayer].characterId].killsInstant){
+      this.markPlayerForWolfAttack(getPlayerIdxByName(this.state.item, AppData.Characters[AppData.Game.LivePlayers), dataFunc);
+    }else if(AppData.Characters[AppData.Game.LivePlayers[AppData.Game.ActivePlayer].characterId].killsInstant){
+      this.killPlayer(getPlayerIdxByName(this.state.item, AppData.Characters[AppData.Game.LivePlayers), dataFunc);
+    }
     if(appData.Game.ActivePlayer+1 < appData.Game.LivePlayers.length){
       appData.Game.ActivePlayer++;
       this.forceUpdate();
       this.setState({ show:false })
     }else{
       appData.Game.ActivePlayer = 0;
+      getKilledByWolf(dataFunc);
       appData.Game.CurrentScreenComponent = Outcome;
     }
     dataFunc(appData)
